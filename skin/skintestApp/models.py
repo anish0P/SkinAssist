9ae6c59f49b413911c1ee0d067b352575a1058b0
anish0P <121811023+anish0P@users.noreply.github.com>
2023-12-01 20:09:44 +0530

    Delete skin directory

--- skin/skintestApp/models.py
+++
@@ -1,8 +0,0 @@
-# models.py in your app
-
-from django.db import models
-
-class Image(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    image = models.ImageField(upload_to='images/')
